962e6da9c7d13d82ac9e29670ee7a0a371afb386
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-11-14 11:48:19 +0800

    Added implementation for 4 AND Gates

--- BackpropagationNeuralNetwork/Forms/AndGateForm.Designer.cs
+++ BackpropagationNeuralNetwork/Forms/AndGateForm.Designer.cs
@@ -32,6 +32,11 @@ partial class AndGateForm
         btnTrainNetwork = new Button();
         btnCreateBPNN = new Button();
         btnTest = new Button();
+        textBoxInput0 = new TextBox();
+        textBoxInput1 = new TextBox();
+        textBoxInput2 = new TextBox();
+        textBoxInput3 = new TextBox();
+        textBoxOutput0 = new TextBox();
         SuspendLayout();
         // 
         // btnGoBack
@@ -52,6 +57,7 @@ partial class AndGateForm
         btnTrainNetwork.TabIndex = 5;
         btnTrainNetwork.Text = "Train Neural Network";
         btnTrainNetwork.UseVisualStyleBackColor = true;
+        btnTrainNetwork.Click += btnTrainNetwork_Click;
         // 
         // btnCreateBPNN
         // 
@@ -61,7 +67,7 @@ partial class AndGateForm
         btnCreateBPNN.TabIndex = 4;
         btnCreateBPNN.Text = "Create BPNN";
         btnCreateBPNN.UseVisualStyleBackColor = true;
-        btnCreateBPNN.Click += this.btnCreateBPNN_Click;
+        btnCreateBPNN.Click += btnCreateBPNN_Click;
         // 
         // btnTest
         // 
@@ -71,12 +77,53 @@ partial class AndGateForm
         btnTest.TabIndex = 6;
         btnTest.Text = "Test";
         btnTest.UseVisualStyleBackColor = true;
+        btnTest.Click += btnTest_Click;
+        // 
+        // textBoxInput0
+        // 
+        this.textBoxInput0.Location = new Point(168, 86);
+        this.textBoxInput0.Name = "textBoxInput0";
+        this.textBoxInput0.Size = new Size(100, 23);
+        this.textBoxInput0.TabIndex = 7;
+        // 
+        // textBoxInput1
+        // 
+        this.textBoxInput1.Location = new Point(168, 142);
+        this.textBoxInput1.Name = "textBoxInput1";
+        this.textBoxInput1.Size = new Size(100, 23);
+        this.textBoxInput1.TabIndex = 8;
+        // 
+        // textBoxInput2
+        // 
+        this.textBoxInput2.Location = new Point(168, 205);
+        this.textBoxInput2.Name = "textBoxInput2";
+        this.textBoxInput2.Size = new Size(100, 23);
+        this.textBoxInput2.TabIndex = 9;
+        // 
+        // textBoxInput3
+        // 
+        textBoxInput3.Location = new Point(168, 263);
+        textBoxInput3.Name = "textBoxInput3";
+        textBoxInput3.Size = new Size(100, 23);
+        textBoxInput3.TabIndex = 10;
+        // 
+        // textBoxOutput0
+        // 
+        textBoxOutput0.Location = new Point(477, 171);
+        textBoxOutput0.Name = "textBoxOutput0";
+        textBoxOutput0.Size = new Size(100, 23);
+        textBoxOutput0.TabIndex = 11;
         // 
         // AndGateForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(800, 450);
+        Controls.Add(textBoxOutput0);
+        Controls.Add(textBoxInput3);
+        Controls.Add(this.textBoxInput2);
+        Controls.Add(this.textBoxInput1);
+        Controls.Add(this.textBoxInput0);
         Controls.Add(btnTest);
         Controls.Add(btnTrainNetwork);
         Controls.Add(btnCreateBPNN);
@@ -84,6 +131,7 @@ partial class AndGateForm
         Name = "AndGateForm";
         Text = "AndGateForm";
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
@@ -91,4 +139,9 @@ partial class AndGateForm
     private Button btnTrainNetwork;
     private Button btnCreateBPNN;
     private Button btnTest;
+    private TextBox textBoxInput0;
+    private TextBox textBoxInput1;
+    private TextBox textBoxInput2;
+    private TextBox textBoxInput3;
+    private TextBox textBoxOutput0;
 }
--- BackpropagationNeuralNetwork/Forms/AndGateForm.cs
+++ BackpropagationNeuralNetwork/Forms/AndGateForm.cs
@@ -3,6 +3,7 @@
 public partial class AndGateForm : Form
 {
     private MainForm parentForm;
+    private NeuralNet neuralNet;
     public AndGateForm(MainForm parentForm)
     {
         InitializeComponent();
@@ -18,40 +19,137 @@ public partial class AndGateForm : Form
 
     private void btnCreateBPNN_Click(Object sender, EventArgs e)
     {
-        //neuralNet = new NeuralNet(2, 100, 1);
+        neuralNet = new NeuralNet(4, 100, 1);
     }
 
     private void btnTrainNetwork_Click(object sender, EventArgs e)
     {
-        //for (int x = 0; x < 100; x++)
-        //{
-        //    neuralNet.setInputs(0, 0.0);
-        //    neuralNet.setInputs(1, 0.0);
-        //    neuralNet.setDesiredOutput(0, 0.0);
-        //    neuralNet.learn();
+        for (int x = 0; x < 100; x++)
+        {
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
 
-        //    neuralNet.setInputs(0, 0.0);
-        //    neuralNet.setInputs(1, 1.0);
-        //    neuralNet.setDesiredOutput(0, 1.0);
-        //    neuralNet.learn();
 
-        //    neuralNet.setInputs(0, 1.0);
-        //    neuralNet.setInputs(1, 0.0);
-        //    neuralNet.setDesiredOutput(0, 1.0);
-        //    neuralNet.learn();
 
-        //    neuralNet.setInputs(0, 1.0);
-        //    neuralNet.setInputs(1, 1.0);
-        //    neuralNet.setDesiredOutput(0, 1.0);
-        //    neuralNet.learn();
-        //}
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 0.0); 
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 0.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 0.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 0.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 0.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 0.0);
+            neuralNet.learn();
+
+
+
+            neuralNet.setInputs(0, 1.0);
+            neuralNet.setInputs(1, 1.0);
+            neuralNet.setInputs(2, 1.0);
+            neuralNet.setInputs(3, 1.0);
+            neuralNet.setDesiredOutput(0, 1.0);
+        }
     }
 
     private void btnTest_Click(object sender, EventArgs e)
     {
-        //neuralNet.setInputs(0, Convert.ToDouble(textBoxInput0.Text));
-        //neuralNet.setInputs(1, Convert.ToDouble(textBoxInput1.Text));
-        //neuralNet.run();
-        //textBoxOutput0.Text = "" + neuralNet.getOuputData(0);
+        neuralNet.setInputs(0, Convert.ToDouble(textBoxInput0.Text));
+        neuralNet.setInputs(1, Convert.ToDouble(textBoxInput1.Text));
+        neuralNet.run();
+        textBoxOutput0.Text = "" + neuralNet.getOuputData(0);
     }
 }
