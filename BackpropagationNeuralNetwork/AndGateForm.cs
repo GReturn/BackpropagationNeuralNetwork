d592e09cdd3875b351627dd5f9894e23f0e8e2a7
Lindrew <66846143+GReturn@users.noreply.github.com>
2025-11-14 11:13:58 +0800

    Added backbone Form for AND Gate form

---
+++ BackpropagationNeuralNetwork/AndGateForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackpropagationNeuralNetwork
+{
+    public partial class AndGateForm : Form
+    {
+        public AndGateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
